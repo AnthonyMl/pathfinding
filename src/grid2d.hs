33ee38b6fc599ff0e2e136b7c25c32122e1217e3
Anthony <mladenov.anthony@gmail.com>
2014-11-18 23:44:55 -0500

    Merging changes from rgrinberg

--- src/grid2d.hs
+++ src/grid2d.hs
@@ -27,7 +27,7 @@ create size start goal = Grid2D size start goal Set.empty
 addObstacles :: Grid2D -> [Vec2] -> Grid2D
 addObstacles (Grid2D size start goal obstacles) newObstacles = Grid2D size start goal (Set.union obstacles (Set.fromList newObstacles))
 
-data Action = Up | Down | Left | Right deriving (Eq)
+data Action = Up | Down | Left | Right deriving (Eq, Enum)
 
 value :: Action -> Vec2
 value Grid2D.Up    = Vec2   0    1
@@ -38,19 +38,21 @@ value Grid2D.Right = Vec2   1    0
 apply :: Vec2 -> Action -> Vec2
 apply vec action = vec + value action
 
+inBounds :: Grid2D -> Vec2 -> Action -> Bool
+inBounds grid (Vec2 _ y) Grid2D.Up    = y < Grid2D.size grid
+inBounds _    (Vec2 _ y) Grid2D.Down  = y >= 0
+inBounds _    (Vec2 x _) Grid2D.Left  = x >= 0
+inBounds grid (Vec2 x _) Grid2D.Right = x < Grid2D.size grid
+
 isValid :: Grid2D -> Vec2 -> Action -> Bool
-isValid grid state action
-  | Set.member newState (Grid2D.obstacles grid) = False
-  | action == Grid2D.Up    = y newState < Grid2D.size grid
-  | action == Grid2D.Down  = y newState >= 0
-  | action == Grid2D.Left  = x newState >= 0
-  | action == Grid2D.Right = x newState < Grid2D.size grid
+isValid grid state action =
+    inBounds grid newState action && not (Set.member newState (Grid2D.obstacles grid))
   where
     newState = apply state action
 
 
 instance Graph Grid2D Vec2 where
-  getNeighbors g state = List.map (apply state) (List.filter (isValid g state) [Grid2D.Up, Grid2D.Down, Grid2D.Left, Grid2D.Right])
+  getNeighbors g state = List.map (apply state) (List.filter (isValid g state) [Grid2D.Up .. Grid2D.Right])
   start                = Grid2D.start
   goal                 = Grid2D.goal
 
